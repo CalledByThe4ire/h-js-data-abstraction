a4716fcecc2f8a48e9cb7dd47047ad91d459e062
Andrey Karamnoff <karamnoff@gmail.com>
2020-06-21 20:54:22 +0300

    add solution for 08-invariants

--- src/rational.js
+++ src/rational.js
@@ -1,40 +1,48 @@
 // @ts-check
 
-import getGcd from './utils.js';
+import getGcd from "./utils.js";
 
 // BEGIN (write your solution here)
-const normalizeRational = (rational) => {
-  //
-};
-
 const makeRational = (numer, denom) => {
-  //
+  const gcd = getGcd(numer, denom);
+  return { numer: numer / gcd, denom: denom / gcd };
 };
 
-const getNumer = (rational) => {
-  //
-};
+const getNumer = ({ numer }) => numer;
 
-const getDenom = (rational) => {
-  //
-};
+const getDenom = ({ denom }) => denom;
 
 const add = (rational1, rational2) => {
-  //
-}
+  if (getDenom(rational1) === getDenom(rational2)) {
+    return makeRational(
+      getNumer(rational1) + getNumer(rational2),
+      getDenom(rational1)
+    );
+  }
+
+  return makeRational(
+    getNumer(rational1) * getDenom(rational2) +
+      getDenom(rational1) * getNumer(rational2),
+    getDenom(rational1) * getDenom(rational2)
+  );
+};
 
 const sub = (rational1, rational2) => {
-  //
-}
+  if (getDenom(rational1) === getDenom(rational2)) {
+    return makeRational(
+      getNumer(rational1) - getNumer(rational2),
+      getDenom(rational1)
+    );
+  }
+
+  return makeRational(
+    getNumer(rational1) * getDenom(rational2) -
+      getDenom(rational1) * getNumer(rational2),
+    getDenom(rational1) * getDenom(rational2)
+  );
+};
 // END
 
 const ratToString = (rat) => `${getNumer(rat)}/${getDenom(rat)}`;
 
-export {
-  makeRational,
-  getNumer,
-  getDenom,
-  add,
-  sub,
-  ratToString,
-};
+export { makeRational, getNumer, getDenom, add, sub, ratToString };
