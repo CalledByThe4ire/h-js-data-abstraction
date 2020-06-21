35159e2d52bf324dfd6bda51c48c91691498f6c3
Andrey Karamnoff <karamnoff@gmail.com>
2020-06-21 20:01:03 +0300

    add hexlet' solution for 07-levels

--- src/rectangle.js
+++ src/rectangle.js
@@ -3,25 +3,29 @@
 import { makeDecartPoint, getX, getY, getQuadrant } from "./points.js";
 
 // BEGIN (write your solution here)
-const makeRectangle = (point, width, height) => {
-  //
-};
+const makeRectangle = (startPoint, width, height) => ({
+  startPoint,
+  width,
+  height,
+});
 
-const getStartPoint = (rectangle) => {
-  //
-};
+const getStartPoint = ({ startPoint }) => startPoint;
 
-const getWidth = (rectangle) => {
-  //
-};
+const getWidth = ({ width }) => width;
 
-const getHeight = (rectangle) => {
-  //
-};
+const getHeight = ({ height }) => height;
 
 const containsOrigin = (rectangle) => {
-  //
+  const point1 = getStartPoint(rectangle);
+  const point2 = makeDecartPoint(
+    getX(point1) + getWidth(rectangle),
+    getY(point1) - getHeight(rectangle),
+  );
+
+  return getQuadrant(point1) === 2 && getQuadrant(point2) === 4;
 };
 
-export { makeRectangle, getStartPoint, getWidth, getHeight, containsOrigin };
+export {
+  makeRectangle, getStartPoint, getWidth, getHeight, containsOrigin,
+};
 // END
